a3301516c58a1c6293f4c6901c435c3c14cb1fdf
Rudra Naik <rudranai@iastate.edu>
2026-02-17 01:15:37 -0600

    I think I buggered up the trees ngl.

--- TestMod/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace MoarLasers
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "MoarLases";
-        public const string PLUGIN_NAME = "More Lases";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
